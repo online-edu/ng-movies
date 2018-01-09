8919d1de4ee29c68f972d2d9f04e62fa116f7c05
Hardik Pithva <hardikpthv@gmail.com>
2018-01-09 21:04:25 +0530

    feat: added PWA capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+
+import { ServiceWorkerModule } from "@angular/service-worker";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { MovieService } from "./movies/movie.service";
 import { MovieModule } from "./movies/movie.module";
 import { AuthGuard } from "./guards/auth-guard.service";
+import { environment } from "environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule,
-    MovieModule
+    MovieModule,
+    ServiceWorkerModule.register("/ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [MovieService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
