649ddfd192a82ccff5f9c35b9afdafae4dea6943
Hardik Pithva <hardikpthv@gmail.com>
2018-07-28 01:07:59 +0530

    feat: update to v6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { ServiceWorkerModule } from "@angular/service-worker";
 
@@ -19,7 +19,7 @@ import { environment } from "environments/environment";
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     MovieModule,
     ServiceWorkerModule.register("/ngsw-worker.js", {
       enabled: environment.production
--- src/app/movies/movie-resolver.service.ts
+++ src/app/movies/movie-resolver.service.ts
@@ -1,18 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs";
 
 import { Movie } from "./movie";
 import { MovieService } from "./movie.service";
 
 @Injectable()
 export class MovieResolver implements Resolve<Movie[]> {
+  constructor(private movieService: MovieService) {}
 
-    constructor(private movieService: MovieService) { }
-
-    resolve(route: ActivatedRouteSnapshot): Observable<Movie[]> {
-        //Subscribe and check movies are exist or not, accrodingly change route
-        return this.movieService.getMovies();
-    }
-
+  resolve(route: ActivatedRouteSnapshot): Observable<Movie[]> {
+    //Subscribe and check movies are exist or not, accrodingly change route
+    return this.movieService.getMovies();
+  }
 }
