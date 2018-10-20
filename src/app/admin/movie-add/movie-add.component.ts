d6624a0b1540e26b69362ff833964d56f894fa1d
Hardik Pithva <hardikpthv@gmail.com>
2018-10-20 13:51:04 +0530

    fix: refactor movie add component

--- src/app/admin/movie-add/movie-add.component.ts
+++ src/app/admin/movie-add/movie-add.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'movie-add',
-  templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.scss']
+  templateUrl: './movie-add.component.html',
+  styleUrls: ['./movie-add.component.scss']
 })
-export class MovieComponent {
+export class MovieAddComponent {
 
   canDeactivate() {
     return confirm("Are you sure want to leave unsaved?");
